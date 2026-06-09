9bc813c833ab954dbb9a54b4ba2c4781e5dbc726
Joshua Cervantes <godotamantedelcafe@gmail.com>
2026-06-09 15:03:59 -0600

    Detección de áreas de luz + barra
    Al tocar una luz, el nivel de detección de los enemigos aumenta, y al alejarse va disminuyendo. Hay una barra que te muestra qué tanto nivel de detección tienes.

--- Flashing Lights/Assets/Scripts/GameManager.cs
+++ Flashing Lights/Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] public bool isPaused = false;
+    [SerializeField] public float SecurityLvl = 0;
     // Start is called before the first frame update
     void Start()
     {
