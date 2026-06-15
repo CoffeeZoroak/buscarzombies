8cc41e769ecba5f9f3e106c1fc11e8455cbc2632
Joshua Cervantes <godotamantedelcafe@gmail.com>
2026-06-15 14:23:39 -0600

    borre la otra barra

--- Flashing Lights/Assets/Scripts/Enemies.cs
+++ Flashing Lights/Assets/Scripts/Enemies.cs
@@ -2,19 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class Enemies : MonoBehaviour
 {
-    [SerializeField] public bool isPaused = false;
-    [SerializeField] public float SecurityLvl = 0;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        SecurityLvl = Mathf.Clamp(SecurityLvl,0, 100);
+        
     }
 }
